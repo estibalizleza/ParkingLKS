59e08ebab52d97f0b046141324853e1c356fc1a7
estibalizleza <eleza002@ikasle.ehu.eus>
2024-06-20 22:24:10 +0200

    add back arrow to RegisterActivity

--- app/src/main/java/com/lksnext/ParkingELeza/view/activity/RegisterActivity.java
+++ app/src/main/java/com/lksnext/ParkingELeza/view/activity/RegisterActivity.java
@@ -3,8 +3,10 @@
     import android.os.Bundle;
 
     import androidx.appcompat.app.AppCompatActivity;
+    import androidx.appcompat.widget.Toolbar;
     import androidx.lifecycle.ViewModelProvider;
 
+    import com.lksnext.parkingplantilla.R;
     import com.lksnext.parkingplantilla.databinding.ActivityRegisterBinding;
     import com.lksnext.ParkingELeza.viewmodel.RegisterViewModel;
 
@@ -22,5 +24,19 @@
 
             //Asignamos el viewModel de register
             registerViewModel = new ViewModelProvider(this).get(RegisterViewModel.class);
+
+            Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar2);
+            setSupportActionBar(myToolbar);
+            // agrega flecha
+            if (getSupportActionBar() != null){
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                getSupportActionBar().setDisplayShowHomeEnabled(true);
+            }
+        }
+
+        @Override
+        public boolean onSupportNavigateUp() {
+            onBackPressed();
+            return true;
         }
     }
